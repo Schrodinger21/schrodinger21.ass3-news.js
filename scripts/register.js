0bc906c0a9d0afbde2faec1d805ebdb11d0a8169
shrodinger <doremon98tybg@gmail.com>
2022-11-08 05:43:08 +0700

    code code

--- scripts/register.js
+++ scripts/register.js
@@ -1,12 +1,35 @@
 "use strict";
 const inputFristName = document.getElementById("input-firstname");
-const inputLastNam = document.getElementById("input-lastname");
+const inputLastName = document.getElementById("input-lastname");
 const inputUserName = document.getElementById("input-username");
 const inputPassword = document.getElementById("input-password");
 const confirmPassword = document.getElementById("input-password-confirm");
 const registerBtn = document.getElementById("btn-submit");
 
 registerBtn.addEventListener("click", function () {
-  const data = {};
-  console.log(data);
+  const user = new User(
+    inputFristName.value,
+    inputLastName.value,
+    inputUserName.value,
+    inputPassword.value
+  );
+  const validate = validateData(user);
 });
+function validateData(user) {
+  if (user.fisrtName.trim().length === 0) {
+    alert("Không được để trống trường này!");
+    return false;
+  } else if (user.lastName.trim().length === 0) {
+    alert("Không được để trống trường này!");
+    return false;
+  } else if (user.password === "" || user.password.length < 8) {
+    alert("Vui lòng nhập Password và Password phải có nhiều hơn 8 kí tự!!!");
+    return false;
+  } else if (
+    user.confirmpassword === "" ||
+    user.password !== confirPassword.value
+  ) {
+    alert("Password và confirmpassword phải giống nhau");
+    return false;
+  }
+}
--- scripts/storage.js
+++ scripts/storage.js
@@ -1,1 +1,37 @@
-'use strict'
+"use strict";
+function getFromStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+// Save data
+function saveToStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+// Lấy data userArr từ Localstorage
+const users = getFromStorage("userArr") ? getFromStorage("userArr") : [];
+
+// convert class Instance
+const userArr = users.map((user) => parseUser(user));
+
+// Lấy data user active
+let userActive = getFromStorage("usderActive")
+  ? parseUser(getFromStorage("userActive"))
+  : null;
+
+// lấy data todoArr từ localstorage
+const todo = getFromStorage("todoArr") ? getFromStorage("todoArr") : [];
+
+// convert class instance
+const todoArr = todo.map((todo) => parseTask(todos));
+
+// Hàm chuyển đổi data
+function parseUser(userData) {
+  const user = new User(
+    userData.firstname,
+    userData.lastname,
+    userData.username,
+    userData.password
+  );
+
+  return user;
+}
